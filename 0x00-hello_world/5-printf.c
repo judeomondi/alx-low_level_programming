e3a9fdbd23ca4e414e48c5edfcc2c53c8b7981cb
judeomondi <judeomondi7@gmail.com>
2022-03-10 05:49:19 -0800

    hellop

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+puts("with the proper grammar, but the outcome is a piece of art");
+return (0);
+}
