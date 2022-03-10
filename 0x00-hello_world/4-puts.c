fb695d98c2270ddb52182fa05994581a96791a5c
judeomondi <judeomondi7@gmail.com>
2022-03-10 05:40:43 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * Return:0 if successful
- * 
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main(void)
+{
+Puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
