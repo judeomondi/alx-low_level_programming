b7e0f13869a06727320d5d1d254d0cbd6006f1d7
judeomondi <judeomondi7@gmail.com>
2022-03-14 09:33:23 -0700

    Task 9 re

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,13 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	putchar(c);
+		putchar(c);
 
-	if (c != '9')
-	{
-																putchar(',');
-																putchar(' ');
-																}
+		if (c != '9')
+		{
+																		putchar(',');
+																		putchar(' ');
+																	}
 	}
 
 	putchar('\n');
