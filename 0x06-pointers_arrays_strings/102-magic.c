26e99b80dff2d07cfecbde67ab5da64fd09509f3
judeomondi <judeomondi7@gmail.com>
2022-03-24 07:30:54 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,47 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- *infinite_add - add two numbers.
- *
- *@n1: first number.
- *@n2: second number.
- *@r: result.
- *@size_r: result size.
- *Return: the addition of n1 and n2.
- */
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int main(void)
 {
-	int add = 0, len1, len2, i, j;
+	int n;
+	int a[5];
+	int *p;
 
-	for (len1 = 0; n1[len1]; len1++)
-	;
-	for (len2 = 0; n2[len2]; len2++)
-	;
-	if (len1 > size_r || len2 > size_r)
-		return (0);
-	len1--;
-	len2--;
-	size_r--;
-	for (i = 0; i < size_r; i++, len1--, len2--)
-	{
-		if (len1 >= 0)
-			add += n1[len1] - '0';
-		if (len2 >= 0)
-			add += n2[len2] - '0';
-		if (len1 < 0 && len2 < 0 && add == 0)
-			break;
-		r[i] = add % 10 + '0';
-		add /= 10;
-	}
-	r[i] = '\0';
-	if (len1 >= 0 || len2 >= 0 || add)
-		return (0);
-	for (i--, j = 0; i > j; i--, j++)
-	{
-		add = r[i];
-		r[i] = r[j];
-		r[j] = add;
-	}
-	return (r);
+	a[2] = 1024;
+	p = &n;
+	/*
+	 *    * write your line of code here...
+	 *       * Remember:
+	 *          * - you are not allowed to use a
+	 *             * - you are not allowed to modify p
+	 *                * - only one statement
+	 *                   * - you are not allowed to code anything else than this line of code
+	 *                      */
+	 *(p + 5) = 98;
+	 /* ...so that this prints 98\n */
+	 printf("a[2] = %d\n", a[2]);
+	 return (0);
 }
